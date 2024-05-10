e71680eeec414b77f60e020f5f2acb0f49646620
jiahao.shi <jiahao.shi@accenture.com>
2024-05-10 09:40:51 +0100

    remove demo-purpose sidebar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -134,7 +134,7 @@ function App() {
     </div>
 
     
-     <FakeSidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+     <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
      <FAQSidebar isOpen={isFAQOpen} setIsOpen={setIsFAQOpen} />
      <RegistrationSideBar isOpen={isRegisterOpen} setIsOpen={setIsRegisterOpen} />
     </div>
